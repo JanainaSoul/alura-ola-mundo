5c59429e884a2dc9dd8462dd64410f514de986a8
Janaina <janainasoul@icloud.com>
2023-05-03 17:15:35 -0300

    criando o roteador

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Inicio from "./paginas/inicio";
 import SobreMim from "./paginas/sobreMim";
 
 
-
 function App() {
-  return 
+  return (
     <BrowserRouter> 
-    
+    <Routes>
+      <Route path="/" element={<Inicio/>} />
+      <Route path="/sobremim" element={<SobreMim/>}/>
+      <Route path="*" element={<div>Página não encontrada</div>}/>
+      </Routes>
     </BrowserRouter>
-  
+  );
 }
 
 export default App;
